eff4d96a0161f63111650f9e83738c3f9044d2e4
dendickys <febri.unquad2014@gmail.com>
2019-10-04 22:02:26 +0700

    Add TImePickerFragment

--- app/src/main/java/com/dendickys/myalarmmanagerexpert/DatePickerFragment.java
+++ app/src/main/java/com/dendickys/myalarmmanagerexpert/DatePickerFragment.java
@@ -5,24 +5,16 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.DatePicker;
-
 import java.util.Calendar;
 import java.util.Objects;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
     DialogDateListener mListener;
